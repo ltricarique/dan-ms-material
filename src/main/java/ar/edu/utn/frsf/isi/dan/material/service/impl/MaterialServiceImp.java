4be9832e8c846053dd60e277d8eb86dba074442a
Leandro Heraldo Tricarique <ltricarique@gmail.com>
2021-06-07 00:27:49 -0300

    Actualización de requerimientos y configuración

--- src/main/java/ar/edu/utn/frsf/isi/dan/material/service/impl/MaterialServiceImp.java
+++ src/main/java/ar/edu/utn/frsf/isi/dan/material/service/impl/MaterialServiceImp.java
@@ -1,121 +1,148 @@
 package ar.edu.utn.frsf.isi.dan.material.service.impl;
 
-import ar.edu.utn.frsf.isi.dan.material.model.*;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ar.edu.utn.frsf.isi.dan.material.model.DetallePedido;
+import ar.edu.utn.frsf.isi.dan.material.model.DetalleProvision;
+import ar.edu.utn.frsf.isi.dan.material.model.Material;
+import ar.edu.utn.frsf.isi.dan.material.model.MovimientoStock;
+import ar.edu.utn.frsf.isi.dan.material.model.Provision;
 import ar.edu.utn.frsf.isi.dan.material.repository.MaterialRepository;
 import ar.edu.utn.frsf.isi.dan.material.repository.MovimientoStockRepository;
 import ar.edu.utn.frsf.isi.dan.material.repository.ProvisionRepository;
 import ar.edu.utn.frsf.isi.dan.material.service.MaterialService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.JmsException;
-import org.springframework.jms.annotation.JmsListener;
-import org.springframework.stereotype.Service;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.ObjectMessage;
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
-public class MaterialServiceImp implements MaterialService {
+public class MaterialServiceImp implements MaterialService
+{
+	private static final Logger LOGGER = LoggerFactory.getLogger(MaterialServiceImp.class);
 
-    @Autowired
-    private MaterialRepository materialRepository;
-    @Autowired
-    private MovimientoStockRepository movimientoStockRepository;
-    @Autowired
-    private ProvisionRepository provisionRepository;
+	@Autowired
+	private MaterialRepository materialRepository;
+	@Autowired
+	private MovimientoStockRepository movimientoStockRepository;
+	@Autowired
+	private ProvisionRepository provisionRepository;
 
-    @JmsListener(destination = "COLA_PEDIDOS")
-    public void handle(Message msg) throws JMSException {
-        //Recibir el mensaje y generar un movimiento de stock y hacer un pedido dado el caso
-        List<DetallePedido> detalles = new ArrayList<>();
-        if (msg instanceof ObjectMessage) {
+	@JmsListener(destination = "COLA_PEDIDOS")
+	public void handle(Message msg) throws JMSException, JsonMappingException, JsonProcessingException
+	{
+		//Recibir el mensaje y generar un movimiento de stock y hacer un pedido dado el caso
+		if (msg instanceof TextMessage)
+		{
+			String json = ((TextMessage) msg).getText();
 
-            Object o = ((ObjectMessage) msg).getObject();
-            detalles = (ArrayList<DetallePedido>) o;
+			ObjectMapper mapper = new ObjectMapper();
+			List<DetallePedido> detalles = Arrays.asList(mapper.readValue(json, DetallePedido[].class));;
+			LOGGER.info("Pedido recibido de COLA_PEDIDOS");
 
-            List<DetalleProvision> detalleProvisions = new ArrayList<>();
+			List<DetalleProvision> detalleProvisions = new ArrayList<>();
 
-            for(DetallePedido d : detalles){
+			for (DetallePedido d : detalles)
+			{
+				Material m = materialRepository.findById(d.getMaterial().getId()).get();
 
-                //Actualizar stock del producto
-                BigDecimal nuevoStock = d.getMaterial().getStockActual().subtract(d.getCantidad());
-                d.getMaterial().setStockActual(nuevoStock);
-                materialRepository.save(d.getMaterial());
+				//Actualizar stock del producto
+				BigDecimal nuevoStock = m.getStockActual().subtract(d.getCantidad());
+				m.setStockActual(nuevoStock);
+				materialRepository.save(m);
 
-                //Crear Movimiento Stock
+				//Crear Movimiento Stock
+				MovimientoStock movimientoStock = new MovimientoStock();
+				movimientoStock.setDetallePedido(d);
+				movimientoStock.setMaterial(m);
+				movimientoStock.setCantidadSalida(d.getCantidad());
+				movimientoStock.setFecha(Instant.now());
+				movimientoStockRepository.save(movimientoStock);
 
-                MovimientoStock movimientoStock = new MovimientoStock();
-                movimientoStock.setDetallePedido(d);
-                movimientoStock.setMaterial(d.getMaterial());
-                movimientoStock.setCantidadSalida(d.getCantidad());
-                movimientoStock.setFecha(Instant.now());
-                movimientoStockRepository.save(movimientoStock);
+				//Crear detalle de provision si el stock es menor al minimo
+				if (nuevoStock.compareTo(m.getStockMinimo()) < 0)
+				{
+					DetalleProvision detalleProvision = new DetalleProvision();
+					detalleProvision.setCantidad(d.getCantidad());
+					detalleProvision.setMaterial(m);
+					detalleProvisions.add(detalleProvision);
+				}
+			}
+			// registrar pedido de provision por cada stock menor al minimo
+			if (!detalleProvisions.isEmpty())
+			{
+				Provision provision = new Provision();
+				provision.setDetalle(detalleProvisions);
+				provision.setFechaProvision(Instant.now());
+				provisionRepository.save(provision);
+			}
+		}
+	}
 
-                //Crear detalle de provision si el stock es menor al minimo
+	@Override
+	public Material guardarMaterial(Material material)
+	{
+		material = materialRepository.save(material);
+		LOGGER.info("Material guardado");
+		return material;
+	}
 
-                if(nuevoStock.compareTo(d.getMaterial().getStockMinimo()) < 0){
-                    DetalleProvision detalleProvision = new DetalleProvision();
-                    detalleProvision.setCantidad(d.getCantidad());
-                    detalleProvision.setMaterial(d.getMaterial());
-                    detalleProvisions.add(detalleProvision);
-                }
+	@Override
+	public Material actualizarMaterial(Material material, Long id)
+	{
+		material = materialRepository.save(material);
+		LOGGER.info("Material actualizado");
+		return material;
+	}
 
-            }
-             // registrar pedido de provision por cada stock menor al minimo
-            if(!detalleProvisions.isEmpty()){
-                Provision provision = new Provision();
-                provision.setDetalle(detalleProvisions);
-                provision.setFechaProvision(Instant.now());
-                provisionRepository.save(provision);
-            }
+	@Override
+	public void bajaMaterial(Long id)
+	{
+		materialRepository.deleteById(id);
+		LOGGER.info("Material eliminado");
+	}
 
-        }
+	@Override
+	public List<Material> listarMateriales()
+	{
+		return materialRepository.findAll();
+	}
 
+	@Override
+	public Material obtenerMaterialPorId(Long id)
+	{
+		return materialRepository.findById(id).get();
+	}
 
-    }
+	@Override
+	public Material obtenerMaterialPorNombre(String nombre)
+	{
+		return materialRepository.findByNombre(nombre);
+	}
 
-    @Override
-    public Material guardarMaterial(Material material) {
+	@Override
+	public List<Material> obtenerMaterialPorRangoStock(BigDecimal min, BigDecimal max)
+	{
+		return materialRepository.findByStockActualBetween(min, max);
+	}
 
-        return materialRepository.save(material);
-    }
-
-    @Override
-    public Material actualizarMaterial(Material material, Long id) {
-        return materialRepository.save(material);
-    }
-
-    @Override
-    public void bajaMaterial(Long id) {
-        materialRepository.deleteById(id);
-    }
-
-    @Override
-    public List<Material> listarMateriales() {
-        return materialRepository.findAll();
-    }
-
-    @Override
-    public Material obtenerMaterialPorId(Long id) {
-        return materialRepository.findById(id).get();
-    }
-
-    @Override
-    public Material obtenerMaterialPorNombre(String nombre) {
-        return materialRepository.findByNombre(nombre);
-    }
-
-    @Override
-    public List<Material> obtenerMaterialPorRangoStock(BigDecimal min, BigDecimal max) {
-        return materialRepository.findByStockActualBetween(min, max);
-    }
-
-    @Override
-    public List<Material> obtenerMaterialPorPrecio(BigDecimal precio) {
-        return materialRepository.findByPrecioUnitario(precio);
-    }
+	@Override
+	public List<Material> obtenerMaterialPorPrecio(BigDecimal precio)
+	{
+		return materialRepository.findByPrecioUnitario(precio);
+	}
 }
